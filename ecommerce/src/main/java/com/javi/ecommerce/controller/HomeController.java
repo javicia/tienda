98c1110fa5e1766fd6c0e166a2af728a06dc5784
Javier <Javier@DESKTOP-NE49AMB>
2023-10-02 23:08:24 +0200

    añadir concentos a vista productos

--- ecommerce/src/main/java/com/javi/ecommerce/controller/HomeController.java
+++ ecommerce/src/main/java/com/javi/ecommerce/controller/HomeController.java
@@ -1,5 +1,7 @@
 package com.javi.ecommerce.controller;
 
+import java.util.Optional;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.javi.ecommerce.model.Producto;
 import com.javi.ecommerce.service.ProductoService;
 
 
@@ -28,7 +31,13 @@ public class HomeController {
 		return"usuario/home";	}
 	
 	@GetMapping("productohome/{id}")
-	public String productoHome(@PathVariable Integer id) {
+	public String productoHome(@PathVariable Integer id, Model model) {
+		log.info("Id producto enviado como parámetro {}", id);
+		Producto producto = new Producto();
+		Optional<Producto> productoOptional = productoService.get(id);
+		producto = productoOptional.get();
+		
+		model.addAttribute("producto", producto);
 		return "usuario/productoHome";
 	}
 }
