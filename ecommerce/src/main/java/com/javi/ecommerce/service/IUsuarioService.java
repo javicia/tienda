19337707702898e3d163101915a1e8e45461d6d8
Javier <Javier@DESKTOP-NE49AMB.home>
2023-10-09 20:35:25 +0200

    funcionalidad para guardar el registro del usuario

--- ecommerce/src/main/java/com/javi/ecommerce/service/IUsuarioService.java
+++ ecommerce/src/main/java/com/javi/ecommerce/service/IUsuarioService.java
@@ -7,6 +7,7 @@ import com.javi.ecommerce.model.Usuario;
 public interface IUsuarioService {
 	
 	Optional<Usuario> findById(Integer id);
+	Usuario save(Usuario usuario);
 	
 	
 }
--- ecommerce/src/main/java/com/javi/ecommerce/service/UsuarioServiceImpl.java
+++ ecommerce/src/main/java/com/javi/ecommerce/service/UsuarioServiceImpl.java
@@ -20,6 +20,12 @@ public class UsuarioServiceImpl implements IUsuarioService{
 		// TODO Auto-generated method stub
 		return usuarioRepository.findById(id);
 	}
+
+	@Override
+	public Usuario save(Usuario usuario) {
+		// TODO Auto-generated method stub
+		return usuarioRepository.save(usuario);
+	}
 	
 	
 	}
