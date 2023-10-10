13533d5823fce1394ed35af4f08f7b056842fecc
Javier <Javier@172.17.86.15>
2023-10-10 21:08:24 +0200

    Implementación vistas detalles compras al user

--- ecommerce/src/main/java/com/javi/ecommerce/service/IOrdenService.java
+++ ecommerce/src/main/java/com/javi/ecommerce/service/IOrdenService.java
@@ -1,6 +1,7 @@
 package com.javi.ecommerce.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import com.javi.ecommerce.model.Orden;
 import com.javi.ecommerce.model.Usuario;
@@ -8,8 +9,10 @@ import com.javi.ecommerce.model.Usuario;
 public interface IOrdenService {
 
 	List<Orden> findAll();
+	Optional<Orden> findById(Integer id);
 	Orden save (Orden orden);
 	 String generarNumeroOrden();
 	 List<Orden> findByUsuario(Usuario usuario);
+	 
 
 }
--- ecommerce/src/main/java/com/javi/ecommerce/service/OrdenServiceImpl.java
+++ ecommerce/src/main/java/com/javi/ecommerce/service/OrdenServiceImpl.java
@@ -2,6 +2,7 @@ package com.javi.ecommerce.service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -57,6 +58,12 @@ public class OrdenServiceImpl implements IOrdenService{
 	public List<Orden> findByUsuario(Usuario usuario) {
 	
 		return ordenRepository.findByUsuario(usuario);
+	}
+
+	@Override
+	public Optional<Orden> findById(Integer id) {
+		// TODO Auto-generated method stub
+		return ordenRepository.findById(id);
 	}
 
 }
